7547c750dfb419e883a82b81c8526b655f7802c0
GiosueSardoInfirri <110346718+GiosueSardoInfirri@users.noreply.github.com>
2024-03-06 13:14:43 +0100

    Final program

--- send_data.py
+++ send_data.py
@@ -1,31 +1,34 @@
 import numpy as np
 import serial
 from input_data import generate_sequence
-
+import time
 
 x_delay = 5
 len_output = 100
 max_number = 127 
 ordered = []
-print_out = False
+print_out = True
 
 sequence = generate_sequence(x_delay=x_delay, len_output=len_output, 
                              max_number=max_number, print_out=print_out)
 
-sequence = sequence.tolist() + [np.max([sequence[-x_delay:-1]])] + [0]
+sequence = sequence.tolist() + [sequence[-1]] + [0]
+
 print(f'Sending the sequence \n{sequence}\n...')
 
 ser = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
 
-for ii in range(x_delay + len(sequence) - 2):
+for ii in range(x_delay + len(sequence)):
 
-    if ii < len(sequence):
-        ser.write(chr(sequence[ii]).encode())
-
-    if ii > x_delay: 
+    if ii > x_delay-1: 
         read_out = ser.read(1)
     
         if read_out:
-            print(f'Read out at iter {ii}: {ord(read_out)}')
+            if print_out:
+                print(f'Read out at iter {ii}: {ord(read_out)}')
             ordered.append(ord(read_out))
+    
+    if ii < len(sequence):
+        ser.write(chr(sequence[ii]).encode())
+
 print(f'Sorted list\n{ordered}')
